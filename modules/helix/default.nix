6fd4bf9d7005fe10f87307d0d2174b059020cf4d
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-02 00:42:13 +0300

    helix: add autotools lsp for makefiles

--- modules/helix/default.nix
+++ modules/helix/default.nix
@@ -1,28 +1,48 @@
 { config, pkgs, ... }:
 {
   environment.variables.EDITOR = "hx";
+  environment.systemPackages = with pkgs; [
+    autotools-language-server
+  ];
 
-  home-manager.sharedModules = [
-    {
+  home-manager.sharedModules = [{
+    programs.helix = {
+      enable = true;
 
-      programs.helix = {
-        enable = true;
-        settings = {
-          theme = "tokyonight";
-          editor = {
-            bufferline = "multiple";
-            color-modes = true;
-            line-number = "relative";
-            cursorline = true;
-          };
-        };
-
-        settings.editor.cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
+      settings = {
+        theme = "tokyonight";
+        editor = {
+          bufferline = "multiple";
+          color-modes = true;
+          line-number = "relative";
+          cursorline = true;
         };
       };
-    }
-  ];
+
+      settings.editor.cursor-shape = {
+        insert = "bar";
+        normal = "block";
+        select = "underline";
+      };
+
+      languages = {
+        language-server = {
+          autotools-language-server = {
+            command = "autotools-language-server";
+          };
+          make-language-server = {
+            command = "make-language-server";
+          };
+        };
+        
+        language = [
+          {
+            name = "make";
+            auto-format = true;
+            language-servers = [ "autotools-language-server" ];
+          }
+        ];
+      };
+    };
+  }];
 }
