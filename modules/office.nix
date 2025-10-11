0807148545d5633008708549cc3c72b274c01522
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-11 22:52:03 +0300

    texlive: add small

--- modules/office.nix
+++ modules/office.nix
@@ -1,5 +1,13 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  tex = (pkgs.texlive.combine {
+      inherit (pkgs.texlive) scheme-basic
+      dvisvgm dvipng # for preview and export as html
+      wrapfig amsmath ulem hyperref capt-of;
+  });
+in {
   environment.systemPackages = with pkgs; [
     libreoffice-fresh
+    tex
   ];
 }
