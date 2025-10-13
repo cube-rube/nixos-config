4a50fb3b71bc76481e9d8fdaa883619759b66e0c
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-13 04:20:23 +0300

    texlive: add xelatex

--- modules/office.nix
+++ modules/office.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }:
 let
   tex = (pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-basic
+      inherit (pkgs.texlive) scheme-full
       dvisvgm dvipng # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of;
+      wrapfig amsmath ulem hyperref capt-of
+      xelatex-dev;
   });
 in {
   environment.systemPackages = with pkgs; [
