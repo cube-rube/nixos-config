7ed0653e0d5cfc2a5c4fb9e2e235a8ae89334763
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-17 18:36:37 +0300

    helix: add fasm

--- modules/helix/config.nix
+++ modules/helix/config.nix
@@ -18,5 +18,18 @@
       name = "c";
       indent = { tab-width = 4; unit = "    "; };
     }
+    {
+      name = "fasm";
+      scope = "source.fasm";
+      file-types = ["fasm"];
+      comment-token = ";";
+      indent = { tab-width = 8; unit = "        "; };
+    }
+  ];
+  grammar = [
+    {
+      name = "fasm";
+      source = { git = "https://github.com/RubixDev/tree-sitter-asm"; rev = "04962e15f6b464cf1d75eada59506dc25090e186"; };
+    }
   ];
 }
