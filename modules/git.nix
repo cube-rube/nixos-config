bde84909d88d9a68f9dbff425894f7537058fab5
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-23 01:25:04 +0300

    git.lazygit: change pager to delta

--- modules/git.nix
+++ modules/git.nix
@@ -39,6 +39,15 @@
           line-numbers-zero-style       = "\"#3b4261\" ";
         };
       };
+
+      programs.lazygit = {
+        enable = true;
+        settings = {
+          git.pagers = [
+            { pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=\"lazygit-edit://{path}:{line}\""; }
+          ];
+        };
+      };
     })
   ];
 }
