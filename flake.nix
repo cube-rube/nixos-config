ea015d7ec91764ca6426e74b2d8b520002728643
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-10-26 02:40:19 +0300

    flake: add agenix

--- flake.nix
+++ flake.nix
@@ -16,14 +16,22 @@
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    agenix.url = "github:ryantm/agenix";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
     zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
 
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -38,10 +46,6 @@
       nix-index-database,
       ...
     }@inputs:
-    let
-      inherit (builtins) readDir;
-      hosts = readDir ./hosts;
-    in
     {
       nixosConfigurations.accord = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -51,7 +55,6 @@
         modules = [
           ./hosts/accord/configuration.nix
           determinate.nixosModules.default
-          agenix.nixosModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -78,6 +81,7 @@
             home-manager.users.cuberub = ./hosts/nixrock/home.nix;
             home-manager.backupFileExtension = "bak";
           }
+          agenix.nixosModules.default
           nix-index-database.nixosModules.nix-index
           { programs.nix-index-database.comma.enable = true; }
           chaotic.nixosModules.default
