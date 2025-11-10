9bc8e51d89c888c1b6b407038feb96846108b1f8
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-11-10 04:03:25 +0300

    direnv: disable timeout warning

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -3,6 +3,12 @@
     programs.direnv = {
       enable = true;
       enableBashIntegration = true;
+      config = {
+        global = {
+          strict_env = true;
+          warn_timeout = 0;
+        };
+      };
       nix-direnv.enable = true;
     };
   }];
