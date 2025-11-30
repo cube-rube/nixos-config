9a74bcd656946952b2254863c7db132664c5056c
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2025-11-30 22:41:48 +0300

    helix: make c indentation two spaces

--- modules/helix/config.nix
+++ modules/helix/config.nix
@@ -18,10 +18,10 @@
       auto-format = true;
       language-servers = [ "autotools-language-server" ];
     }
-    {
-      name = "c";
-      indent = { tab-width = 4; unit = "    "; };
-    }
+    # {
+    #   name = "c";
+    #   indent = { tab-width = 4; unit = "    "; };
+    # }
     {
       name = "nix";
       auto-format = false;
