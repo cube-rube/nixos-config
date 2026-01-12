6787bb5a34794ae461ab0ee6a8b909272dd80721
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2026-01-12 16:29:09 +0300

    kernel: replace with zen

--- hosts/nixrock/kernel.nix
+++ hosts/nixrock/kernel.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   # boot.kernelPatches = [ {
   #   name = "vt-less-config";
   #   patch = null;
