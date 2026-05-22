a4063b999d262de9a7012662ce982cb7a8bb0ff2
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2026-05-22 15:50:58 +0300

    nvidia: switch to 580

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -7,7 +7,7 @@
         powerManagement.enable = true;
         open = false; # old gpu
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
       };
 
       services.xserver.videoDrivers = [ "nvidia" ];
