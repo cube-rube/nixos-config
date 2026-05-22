9ed62c90bb7d15caea21d2b7c187ca188416fed0
Cub Rub <111195474+cube-rube@users.noreply.github.com>
2026-05-22 16:06:29 +0300

    bat: add

--- modules/term/bat.nix
+++ modules/term/bat.nix
@@ -1,22 +1,32 @@
 {
   # ???????????
-  flake.modules.nixos.batman =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = [
-        pkgs.bat
-        pkgs.bat-extras.batman
-      ];
-      environment.shellAliases.man = "batman";
-    };
+  # flake.modules.nixos.batman =
+  #   { pkgs, ... }:
+  #   {
+  #     environment.systemPackages = [
+  #       pkgs.bat
+  #       pkgs.bat-extras.batman
+  #     ];
+  #     environment.shellAliases.man = "batman";
+  #   };
   flake.modules.hjem.batman =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
+    let
+      inherit (lib.meta) getExe;
+    in
     {
       packages = [
         pkgs.bat
         pkgs.bat-extras.batman
       ];
 
-      programs.nushell.aliases.man = "batman";
+      xdg.config.files."bat/config".text = ''
+        --theme=base16
+      '';
+
+      programs.nushell.aliases = {
+        man = getExe pkgs.bat-extras.batman;
+        cat = getExe pkgs.bat;
+      };
     };
 }
