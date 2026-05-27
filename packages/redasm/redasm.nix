4eef18d583f2322a0e947bf974b2dc0f3bc9d918
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-05-27 21:58:00 +0300

    redasm: remove

--- packages/redasm/redasm.nix
+++
@@ -1,9 +0,0 @@
-{
-  perSystem =
-    { config, pkgs, ... }:
-    {
-      packages.qhexview = pkgs.callPackage ./qhexview/_package.nix { };
-      packages.libredasm = pkgs.callPackage ./libredasm/_package.nix { };
-      packages.redasm = pkgs.callPackage ./_package.nix { inherit (config.packages) qhexview libredasm; };
-    };
-}
