be8a96688f65e86c299240378eed6774ec7c5220
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-05-31 20:09:59 +0300

    locale: change to en_US

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -1,18 +1,21 @@
 {
   flake.modules.nixos.locale = {
     time.timeZone = "Europe/Moscow";
-    i18n.defaultLocale = "ru_RU.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
+    # i18n.extraLocales = ["ru_RU.UTF-8/UTF-8"];
     i18n.extraLocaleSettings = {
-      LC_ALL = "ru_RU.UTF-8";
-      LC_ADDRESS = "ru_RU.UTF-8";
-      LC_IDENTIFICATION = "ru_RU.UTF-8";
-      LC_MEASUREMENT = "ru_RU.UTF-8";
-      LC_MONETARY = "ru_RU.UTF-8";
-      LC_NAME = "ru_RU.UTF-8";
-      LC_NUMERIC = "ru_RU.UTF-8";
-      LC_PAPER = "ru_RU.UTF-8";
-      LC_TELEPHONE = "ru_RU.UTF-8";
-      LC_TIME = "ru_RU.UTF-8";
+      # LC_ADDRESS = "ru_RU.UTF-8";
+      LC_COLLATE = "C.UTF-8"; # ordering rules
+      # LC_CTYPE = "en_US.UTF-8";
+      # LC_IDENTIFICATION = "ru_RU.UTF-8";
+      # LC_MEASUREMENT = "ru_RU.UTF-8";
+      # LC_MESSAGES = "en_US.UTF-8";
+      # LC_MONETARY = "ru_RU.UTF-8";
+      # LC_NAME = "ru_RU.UTF-8";
+      # LC_NUMERIC = "ru_RU.UTF-8";
+      # LC_PAPER = "ru_RU.UTF-8";
+      # LC_TELEPHONE = "ru_RU.UTF-8";
+      # LC_TIME = "ru_RU.UTF-8";
     };
     services.xserver.xkb = {
       layout = "us,ru";
