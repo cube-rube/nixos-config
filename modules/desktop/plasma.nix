f2524ca8080c71cbbcc7e0feacc4a60bc22d70e7
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-05-31 20:09:59 +0300

    plasma: switch back to sddm

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -1,6 +1,7 @@
 {
   flake.modules.nixos.plasma = {
-    services.displayManager.plasma-login-manager.enable = true;
+    # services.displayManager.plasma-login-manager.enable = true;
+    services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
   };
 }
