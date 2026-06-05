4e1982e48330f66e58b6c303d891d6135135909c
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-06-05 17:27:03 +0300

    locale: change back

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -1,12 +1,12 @@
 {
   flake.modules.nixos.locale = {
     time.timeZone = "Europe/Moscow";
-    i18n.defaultLocale = "en_US.UTF-8";
-    # i18n.extraLocales = ["ru_RU.UTF-8/UTF-8"];
+    i18n.defaultLocale = "ru_RU.UTF-8";
+    i18n.extraLocales = ["en_US.UTF-8/UTF-8"];
     i18n.extraLocaleSettings = {
       # LC_ADDRESS = "ru_RU.UTF-8";
       LC_COLLATE = "C.UTF-8"; # ordering rules
-      # LC_CTYPE = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
       # LC_IDENTIFICATION = "ru_RU.UTF-8";
       # LC_MEASUREMENT = "ru_RU.UTF-8";
       # LC_MESSAGES = "en_US.UTF-8";
