7826ec65ff533758881e7abf04b4d5047dc61ce1
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-06-17 15:54:46 +0300

    kmscon: fix option

--- modules/kmscon.nix
+++ modules/kmscon.nix
@@ -4,10 +4,10 @@
     {
       services.kmscon = {
         enable = true;
-        hwRender = true;
         useXkbConfig = true;
         config = {
           font-name = "JetBrainsMono Nerd Font Mono";
+          hwaccel = true;
         };
       };
       fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
