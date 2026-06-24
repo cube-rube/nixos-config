9b404333b8deb67f8d1be26eb4e42cc6fe72d17f
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-06-24 17:39:50 +0300

    kernel: bore + lto

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -6,6 +6,8 @@
       nixpkgs.overlays = [
         inputs.nix-cachyos-kernel.overlays.default
       ];
-      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-x86_64-v3;
+      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v3;
+
+      boot.kernel.sysctl."kernel.sched_bore" = 1;
     };
 }
