730084e67e05e0980dc779ae3356e18a9772df4d
Cuberub <111195474+cube-rube@users.noreply.github.com>
2026-07-25 22:39:50 +0300

    syncthing: change user

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,5 +1,71 @@
 {
-  flake.modules.nixos.syncthing = {
-    services.syncthing.enable = true;
-  };
+  flake.modules.nixos.syncthing =
+    let
+      primaryUser = "cuberub";
+    in
+    {
+      services.syncthing = {
+        enable = true;
+        user = primaryUser;
+        dataDir = "/home/${primaryUser}";
+        configDir = "/home/${primaryUser}/.config/syncthing";
+      };
+    };
+
+   # idk man
+  
+   # flake.modules.hjem.syncthing =
+   #   { lib, pkgs, ... }:
+   #   let
+   #     inherit (lib.lists) singleton;
+   #     inherit (lib.cli) toCommandLineShellGNU;
+   #     inherit (lib.meta) getExe;
+   
+   #     package = pkgs.syncthing;
+   #   in
+   #   {
+   #     packages = singleton package;
+  
+   #     systemd.services = {
+   #       syncthing = {
+   #         description = "Syncthing - Open Source Continuous File Synchronization";
+   #         documentation = "man:syncthing(1)";
+   #         after = singleton "network.target";
+   #         wantedBy = singleton "default.target";
+   #         startLimitIntervalSec = 60;
+   #         startLimitBurst = 4;
+  
+   #         environment = {
+   #           STLOGFORMATTIMESTAMP = "";
+   #           STLOGFORMATLEVELSTRING = "false";
+   #           STLOGFORMATLEVELSYSLOG = "true";
+   #         };
+  
+   #         serviceConfig = {
+   #           ExecStart =
+   #             let
+   #               args = toCommandLineShellGNU { } {
+   #                 "gui-address" = "127.0.0.1:8384";
+   #                 "no-browser" = true;
+   #                 "no-restart" = true;
+   #                 "no-upgrade" = true;
+   #               };
+   #             in
+   #             "${getExe package} serve ${args}";
+   #           Restart = "on-failure";
+   #           SuccessExitStatus = "3 4";
+   #           RestartForceExitStatus = "3 4";
+             
+   #           SystemCallArchitectures = "native";
+   #           MemoryDenyWriteExecute = true;
+   #           NoNewPrivileges = true;
+
+   #           LockPersonality = true;
+   #           PrivateUsers = true;
+   #           RestrictNamespaces = true;
+   #           SystemCallFilter = "@system-service";
+   #         };
+   #       };
+   #     };
+   #   };
 }
